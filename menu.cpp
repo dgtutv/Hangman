6b1da221985c7ba9fc9b5fc00d5735676cdca29b
dgtutv <dgtutv@gmail.com>
2021-12-16 01:43:20 -0800

    Created a way to navigate through menus

--- menu.cpp
+++ menu.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//contains a list of menus, allowing for us to navigate menus and keep track of where we are via linked list
 class menu{
     private:
-    struct menuObject{
-        menuObject(string name, bool isMenu):name(name), isMenu(isMenu){}
-        string name;
-        bool isMenu;
-        string getName(){
-            return name;
-        }
-        bool isMenuCheck(){
-            return isMenu;
-        }
-    };
-    vector<menuObject> options;
+    //we will keep track of which menus this one can access via a vector and a parent pointer
+    vector<menu*> options;
     string name;
+    menu* parent;
     public:
-    void insert(menuObject obj){
+    string getName(){
+        return name;
+    }
+    void insert(menu* obj){
         options.push_back(obj);
     }
-    menu(string str):name(str){}
+    //print all the menus this one connects to, i.e. the menus the user can access from here
+    void print(){
+        int size=options.size();
+        cout<<". return to "<<parent->getName();
+        for(int i=0; i<size; i++){
+            cout<<i+2<<". "<<options[i]->getName()<<endl;
+        }
+    }
+    menu(string str, menu *parentIn):name(str){
+        parent=parentIn;
+    }
 };
